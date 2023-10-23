86571a0f2adf99eed28a9602f46097aefafad450
chadjowens <chadjowens@gmail.com>
2023-10-23 18:28:12 -0500

    Returned code back to square 1 to start fresh on code for playing multiple rounds

--- logic.js
+++ logic.js
@@ -2,16 +2,17 @@ console.log("Hello World!!");
 
 let playerInput = "Rock";
 let playerSelection = playerInput.toLowerCase();
-// const playerSelection = getPlayerChoice();
+// const playerSelection = getPlayerChoice();  
 
 // Holding string value returned by function into declared variable
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
+let computerSelection = getComputerChoice();
+console.log("computerSelection= ", computerSelection); 
 
 // Making a number for computer choice and converting it to an equivalent string
 function getComputerChoice() {
     let computerChoice = Math.floor((Math.random() * 3) + 1);
-    console.log(computerChoice);
+    console.log("computerChoice= ", computerChoice);
+    console.log("///////////////");
     
     // Converting number to equivalent string
     if (computerChoice === 1) {
@@ -30,20 +31,10 @@ function getComputerChoice() {
 
 // //Getting the user selection input
 // function getPlayerChoice() {
-//     let choice = userInput.toLowerCase();
-//         if (userInput === "Rock" || userInput === "Paper" || userInput === "Scissors") {
-//             return userInput;
-//         } else {
-//             return "That choice doesn't exist"
-//         }
 //     }
 
-// Declared function for playRound and takes in 2 parameters (playerSelection and computerSelection)
 function playRound(playerSelection, computerSelection) {
-    // console.log("Play Round!!");
-    console.log("Play Round!!");
-    // console.log(computerChoice);
-
+    
     if (`${playerSelection}` === "rock" && computerSelection === "rock") {
         //console.log("It's a Draw!");
         return "It's a Draw!";
@@ -51,40 +42,21 @@ function playRound(playerSelection, computerSelection) {
         //console.log("Sorry, you lose!");
         return "Sorry, you lose!";
     } else if (`${playerSelection}` === "rock" && computerSelection === "scissors") {
-       // console.log("Yay, you win!");
+    // console.log("Yay, you win!");
         return "Yay, you win!";
     } else {
         return "Input not valid."
     }
+    
 }
 console.log(playRound(playerSelection, computerSelection));
 
 
-// function game() {
-    // console.log("Hello World!!");
-    // console.log("Play Round!!");
-    // // console.log(computerChoice);
-    // console.log(computerSelection);
-    // for (i = 1, i <= 4, i++);
-    // playRound(playerSelection, getComputerChoice());
-    // console.log(playerSelection, computerSelection);
 
-    // playRound(playerSelection, getComputerChoice());
-    // console.log(playerSelection, computerSelection);
-    
-    // playRound(playerSelection, getComputerChoice());
-    // console.log(playerSelection, computerSelection);
-   
-    // playRound(playerSelection, getComputerChoice());
-    // console.log(playerSelection, computerSelection);
+// console.log("round 1");
+// console.log("computerSelection= ", computerSelection);
+// console.log("value from getComputerChoice= ", getComputerChoice());
+// console.log("computerSelection= ", computerSelection);
+// console.log("value from getComputerChoice= ", getComputerChoice());
+// console.log("computerSelection= ", computerSelection);
 
-    // playRound(playerSelection, getComputerChoice());
-    // console.log(playerSelection, computerSelection);
-   
-    // console.log(getComputerChoice(playRound(playerSelection, computerSelection))); 
-    // console.log(getComputerChoice(playRound(playerSelection, computerSelection))); 
-    // playRound();
-// }
-
-// game();
-// console.log(playRound(playerSelection, computerSelection));
