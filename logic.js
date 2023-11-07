208c13dbdad048b5b8cfdb148b7cc4a2732fea9e
chadjowens <chadjowens@gmail.com>
2023-11-07 14:33:42 -0600

    Added a restart button after game is won.

--- logic.js
+++ logic.js
@@ -4,7 +4,6 @@ let scorePlayer = 0;
 let scoreComputer = 0;
 let scoreDraws = 0;
 
-
 const rockBtn = document.querySelector("#rockBtn");
 const paperBtn = document.querySelector('#paperBtn');
 const scissorsBtn = document.querySelector('#scissorsBtn');
@@ -12,6 +11,7 @@ const outcomeDiv = document.querySelector('.outcome');
 const container = document.querySelector('.container');
 const scorePlayerSpan = document.querySelector('.player-score');
 const scoreComputerSpan = document.querySelector('.computer-score');
+const container2 = document.querySelector('.container-2')
 
 // Making a number for computer choice and converting it to an equivalent string
 const options = ["rock", "paper", "scissors"];
@@ -21,14 +21,12 @@ function getComputerChoice() {
     return choice;
 }
 
-// Calculate number of draws or wins for each round
+// Calculate number of draws / wins / loses for each round
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
         scoreDraws++;
-        // console.log("It's a Tie");
-        // console.log("Draws: " + scoreDraws);
         const p = document.createElement('p');
-        p.innerText = `You tied! You both picked ${playerSelection}`;
+        p.innerText = `You Tied! You both picked ${playerSelection}`;
         outcomeDiv.appendChild(p);
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
@@ -36,17 +34,13 @@ function playRound(playerSelection, computerSelection){
         (playerSelection === "paper" && computerSelection === "rock")
     ) {
         scorePlayer++
-        // console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        // console.log("Player Wins: " + scorePlayer);
         const p = document.createElement('p');
         p.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
         outcomeDiv.appendChild(p);
     } else {
         scoreComputer++;
-        // console.log(`You Lose! Computer Wins! ${computerSelection} beats ${playerSelection}`)
-        // console.log("Computer Wins: " + scoreComputer);
         const p = document.createElement('p');
-        p.innerText = `You Lose! Computer Wins! ${computerSelection} beats ${playerSelection}`;
+        p.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
         outcomeDiv.appendChild(p);
     }
 }
@@ -59,21 +53,43 @@ const updateScore = (scorePlayer, scoreComputer) => {
 const checkForWinner = (scorePlayer, scoreComputer) => {
     if (scorePlayer === 5) {
         const h2 = document.createElement('h2');
+        const restartBtn = document.createElement('button');
+
         h2.classList.add('player-won');
         h2.innerText = `You Won, ${scorePlayer} to ${scoreComputer} Great job beating the computer!`;
         outcomeDiv.appendChild(h2);
         document.querySelector("#rockBtn").disabled="disabled";
         document.querySelector("#paperBtn").disabled="disabled";
         document.querySelector("#scissorsBtn").disabled="disabled";
+
+        restartBtn.id = 'restart-btn';
+        restartBtn.innerHTML = 'Restart'
+        outcomeDiv.appendChild(restartBtn);
+        
+        restartBtn.addEventListener("click", () => {
+            window.location.reload();
+            });
+        
         
     } else if (scoreComputer === 5) {
         const h2 = document.createElement('h2');
+        const restartBtn = document.createElement('button');
+
         h2.classList.add('computer-won');
         h2.innerText = `Computer Won, ${scoreComputer} to ${scorePlayer} Too bad -- you Lost to the Computer!`;
         outcomeDiv.appendChild(h2);
         document.querySelector("#rockBtn").disabled="disabled";
         document.querySelector("#paperBtn").disabled="disabled";
         document.querySelector("#scissorsBtn").disabled="disabled";
+
+        restartBtn.id = 'restart-btn';
+        restartBtn.innerHTML = 'Restart'
+        outcomeDiv.appendChild(restartBtn);
+
+        restartBtn.addEventListener("click", () => {
+            window.location.reload();
+            });
+        
     }
 }
 
@@ -81,8 +97,6 @@ rockBtn.addEventListener("click", () => {
     const computerSelection = getComputerChoice()
         const playerSelection = "rock"
         playRound(playerSelection, computerSelection)
-        // console.log("Rock Button");
-        // console.log(`computer Choice: ${computerSelection}`);
         checkForWinner(scorePlayer, scoreComputer);
         updateScore(scorePlayer, scoreComputer);
     });
@@ -91,8 +105,6 @@ paperBtn.addEventListener("click", () => {
     const computerSelection = getComputerChoice()
         const playerSelection = "paper"
         playRound(playerSelection, computerSelection)
-        // console.log("Paper Button");
-        // console.log(`computer Choice: ${computerSelection}`);
         updateScore(scorePlayer, scoreComputer);
         checkForWinner(scorePlayer, scoreComputer);
     });
@@ -101,8 +113,6 @@ scissorsBtn.addEventListener("click", () => {
     const computerSelection = getComputerChoice()
         const playerSelection = "scissors"
         playRound(playerSelection, computerSelection)
-        // console.log("Scissors Button")
-        // console.log(`computer Choice: ${computerSelection}`);
         updateScore(scorePlayer, scoreComputer);
         checkForWinner(scorePlayer, scoreComputer); 
     });
